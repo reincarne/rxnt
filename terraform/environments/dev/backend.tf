97fe5f8a5e0a447c574f7fe4d967254ab165f3bb
Alex <alex@DESKTOP-NLFI8N0.localdomain>
2026-03-07 14:16:39 -0500

    changed backend to azure

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,5 +1,8 @@
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "rxnt_storage"
+    container_name       = "tfstate"
+    key                  = "dev/terraform.tfstate"
   }
 }
